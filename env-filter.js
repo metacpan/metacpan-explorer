1021960e0a8d2a75689f865ddfb4c9b72080d943
Randy Stauner <randy@magnificent-tears.com>
2014-09-28 12:18:15 -0700

    Add cache buster querystring to built files
    and simplify the env blocks.

--- env-filter.js
+++ env-filter.js
@@ -2,8 +2,10 @@ var text = "";
 process.stdin.resume();
 process.stdin.on("data", function(data){ text += data; });
 process.stdin.on("end", function(){
+  var bust = (new Date()).getTime().toString(16);
   process.stdout.write(
     text
+      .replace(/\{\{bust\}\}/g, bust)
       .replace(/(?:<!--\s+)?\{\{(dev|build)\}\}((?:.|\n)+?)?\{\{\/\1\}\}(?:\s+-->)?/g, function(){
         // Enable the contents of the 'build' sections and remove the 'dev' sections.
         return RegExp.$1 == 'build' ? RegExp.$2 : '';
