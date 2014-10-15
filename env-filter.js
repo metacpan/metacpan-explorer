77020b9a7e9a576e45d8117087286e394088e29d
Randy Stauner <randy@magnificent-tears.com>
2014-10-14 21:51:29 -0700

    Shorten cache-buster to make it more interesting

--- env-filter.js
+++ env-filter.js
@@ -2,7 +2,7 @@ var text = "";
 process.stdin.resume();
 process.stdin.on("data", function(data){ text += data; });
 process.stdin.on("end", function(){
-  var bust = (new Date()).getTime().toString(16);
+  var bust = (new Date()).getTime().toString(36);
   process.stdout.write(
     text
       .replace(/\{\{bust\}\}/g, bust)
