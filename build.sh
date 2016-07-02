283296eb98bceb8d7aa6ed20dcb5507d48f9405e
Randy Stauner <randy@magnificent-tears.com>
2016-07-01 21:02:48 -0700

    Move build steps into npm scripts
    It's more consistent for the npm-initiated
    and allows skipping the install step.

--- build.sh
+++ build.sh
@@ -3,7 +3,4 @@
 npm install
 export PATH=`npm bin || echo node_modules/.bin`:$PATH
 
-r.js -o app.build.js
-lessc --clean-css app/styles/main.less > build/styles.css
-
-node env-filter.js < app/index.html > build/index.html
+npm run build
